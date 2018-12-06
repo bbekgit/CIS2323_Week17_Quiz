f16293aa9f9ccd788c4b6e14103a08a13f11e6e4
bbekjava <bibek.gupta@okstate.edu>
2018-12-05 18:19:57 -0600

    Initial commit

---
+++ NonFiction.java
@@ -0,0 +1,15 @@
+public class NonFiction extends Book {
+
+public NonFiction(String title) {
+  super(title);
+  setPrice();
+}
+
+public void setPrice() {
+  price = 37.99;
+}
+
+void showInfo() {
+  System.out.println("NonFiction - Title: " + getTitle() + " $"+getPrice());
+}
+}
---
+++ UseBook.java
@@ -0,0 +1,9 @@
+public class UseBook {
+
+public static void main(String[] args) {
+  Fiction aSimplePlan = new Fiction("A Simple Plan");
+  NonFiction javaIntro = new NonFiction("Introduction to Java");
+  aSimplePlan.showInfo();
+  javaIntro.showInfo();
+}
+}
